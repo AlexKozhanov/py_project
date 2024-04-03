d429fe1338a652b8a3f1573514ddd9bcb0dda9d9
Admin <geniusalex59@gmail.com>
2024-04-03 22:22:50 +0300

    second commit

--- funcs.py
+++ funcs.py
@@ -1,6 +1,8 @@
 def summ_two_namber(a,b):
-    # c = a + b
     return print(f"Сумма переменных равна {a+b}")
+
+def subtraction_two_namber(a,b):
+    return print(f"Разность переменных равна {a - b}")
 
 def main():
     print("Введите переменную a")
@@ -8,6 +10,7 @@ def main():
     print("Введите переменную b")
     B = int(input())
     summ_two_namber(A,B)
+    subtraction_two_namber(A,B)
 
 if __name__ == '__main__':
     main()
