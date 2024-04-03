3c5cdd60a561a16785b7721ac52317f3c201717d
Admin <geniusalex59@gmail.com>
2024-04-03 21:50:47 +0300

    first commit

---
+++ funcs.py
@@ -0,0 +1,13 @@
+def summ_two_namber(a,b):
+    # c = a + b
+    return print(f"Сумма переменных равна {a+b}")
+
+def main():
+    print("Введите переменную a")
+    A = int(input())
+    print("Введите переменную b")
+    B = int(input())
+    summ_two_namber(A,B)
+
+if __name__ == '__main__':
+    main()
