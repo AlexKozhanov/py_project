04196f29b92cf760921eb9e6b979ce2671208558
Admin <geniusalex59@gmail.com>
2024-04-03 22:33:24 +0300

    fourth commit

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+def summ_two_namber(a,b):
+    return print(f"Сумма переменных равна {a+b}")
+
+def subtraction_two_namber(a,b):
+    return print(f"Разность переменных равна {a - b}")
+
+
 def main():
-    pass
+    print("Введите переменную a")
+    A = int(input())
+    print("Введите переменную b")
+    B = int(input())
+    summ_two_namber(A, B)
+    subtraction_two_namber(A, B)
 
 if __name__ == '__main__':
     main()
